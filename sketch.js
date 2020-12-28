b27cae09b85b6e33e87cd4ecea47b27f8494b3f8
Neel-0423 <71958781+Neel-0423@users.noreply.github.com>
2020-12-28 15:33:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+//Create variables here
+var dog, happyDog, database, foodS, foodStock, dogI, hdogI;
+
+function preload()
+{
+  //load images here
+  dogI = loadImage("Images\dogImg.png");
+  hdogI = loadImage("Images\doglmg1.png");
+}
+
+function setup() {
+	createCanvas(500, 500);
+  
+  dog= createSprite(250,250);
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
